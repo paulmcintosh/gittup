eeb7cd2d78c0f7b23f53d7971b9cf0fb8b383622
Steve Austin <steve@austin.com>
2015-09-04 16:28:45 +0100

    Default commit

---
+++ gitlog.sh
@@ -0,0 +1,5 @@
+git add .
+git commit -m 'Default commit'
+git push -u origin dev
+git log --oneline --decorate --all --graph
+
--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include "square.h"
 
 int main(void)
 {
-	printf("Hello, world!\n");
+	printf("Hi, everybody!\n");
+	printf("Five squared is: %i\n", square(5));
 	return 0;
 }
